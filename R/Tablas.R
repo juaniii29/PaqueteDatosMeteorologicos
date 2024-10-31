b59ba4e99811ad071bdb4ccda390e4e12c69d873
juaniii29 <juanicastillo2006@gmail.com>
2024-10-31 16:53:33 -0300

    documentacion funcion de tabla

--- R/Tablas.R
+++ R/Tablas.R
@@ -1,3 +1,11 @@
+#' Tabla de resumen
+#'
+#' @param nombre_archivo
+#'
+#' @return
+#' @export
+#'
+#' @examples
 tabla_resumen_temperatura <- function(nombre_archivo) {
 
   ruta <- paste0("data/", nombre_archivo, ".csv.rda")
@@ -8,17 +16,16 @@ tabla_resumen_temperatura <- function(nombre_archivo) {
   data <- get(nombre_objeto)
 
   if ("temperatura_abrigo_150cm" %in% colnames(data)) {
-    # Crear un resumen sin agrupación (asumiendo que es para una sola estación)
     resumen <- data %>%
       dplyr::summarise(
         promedio_temperatura = mean(temperatura_abrigo_150cm, na.rm = TRUE),
         desviacion_estandar = sd(temperatura_abrigo_150cm, na.rm = TRUE)
       )
   } else {
-    stop("La columna 'temperatura_abrigo_150cm' no está presente en los datos.")
+    stop("La columna 'temperatura_abrigo_150cm' no esta presente en los datos.")
   }
 
   return(resumen)
 }
 
-tabla_resumen_temperatura('NH0437')
+
